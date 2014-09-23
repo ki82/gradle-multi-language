604d561666a0225430d15fb2134c91bdba8619f4
ki82 <akerstrom.christian@gmail.com>
2014-09-23 09:27:18 +0200

    Adding cpp and scala example code

---
+++ src/Batman/cpp/Batman.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Batman.h"
+
+Batman::Batman ()
+{
+
+}
+
+int main(void) {
+    puts("Hello World!!!");
+    return EXIT_SUCCESS;
+}
---
+++ src/main/java/superheroes/Superman.java
@@ -0,0 +1,5 @@
+package superheroes;
+
+public class Superman {
+
+}
